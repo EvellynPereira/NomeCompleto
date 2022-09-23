9e2af1c0c7863c3fa7bc3f19a038db3255f29f7c
evellyn <evellynl464@gmail.com>
2022-09-22 21:23:21 -0300

    resolução da atividade

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.WriteLine("Digite seu nome......: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu sobrenonome......: ");
+sobrenome = Console.ReadLine();
+
+Console.WriteLine($"{nome} {sobrenome}");
+Console.Write($"{sobrenome.ToUpper()}, {nome}");
